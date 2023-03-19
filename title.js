e958adcda7c5af34fd61117b8392381f0392000d
low-resource-indic-translation <128242726+low-resource-indic-translation@users.noreply.github.com>
2023-03-19 23:14:51 +0530

    Update title.js

--- title.js
+++ title.js
@@ -1,11 +1,10 @@
 document.write('\
       <h2> \
-        EMNLP 2021 <br> \
-        SIXTH CONFERENCE ON<br> \
-        MACHINE TRANSLATION (WMT21) \
+        EMNLP 2023 <br> \
+        EIGHT CONFERENCE ON<br> \
+        MACHINE TRANSLATION (WMT23) \
       </h2> \
       <h3> \
-        November 10-11, 2021<br> \
-        Punta Cana (Dominican Republic) and Online\
+        TBA, 2023<br> \
       </h3>\
 ');
