e3089ea962d37a5488e14168aabf242c36af38a4
low-resource-indic-translation <128242726+low-resource-indic-translation@users.noreply.github.com>
2023-03-19 22:12:50 +0530

    Add files via upload

---
+++ title.js
@@ -0,0 +1,11 @@
+document.write('\
+      <h2> \
+        EMNLP 2021 <br> \
+        SIXTH CONFERENCE ON<br> \
+        MACHINE TRANSLATION (WMT21) \
+      </h2> \
+      <h3> \
+        November 10-11, 2021<br> \
+        Punta Cana (Dominican Republic) and Online\
+      </h3>\
+');
