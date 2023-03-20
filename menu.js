51cda6d49b2e0e21cc1e312e4d303c3e18e12051
low-resource-indic-translation <128242726+low-resource-indic-translation@users.noreply.github.com>
2023-03-20 22:58:52 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,54 +1,37 @@
-/*
 document.write('\
-  [<A HREF="/wmt21/index.html">HOME</A>] \
-  [<A HREF="/wmt21/program.html">SCHEDULE</A>] \
-  [<A HREF="/wmt21/papers.html">PAPERS</A>] \
-  [<A HREF="/wmt21/authors.html">AUTHORS</A>] \
-  [<A HREF="/wmt21/results.html">RESULTS</A>] \
-  <br>\
-  TRANSLATION TASKS: \
-  [<A HREF="/wmt21/translation-task.html">NEWS</A>] \
-  [<A HREF="/wmt21/similar.html">SIMILAR LANGUAGES</A>] \
-  [<A HREF="/wmt21/biomedical-translation-task.html">BIOMEDICAL</A>] \
-  [<A HREF="/wmt21/multilingualHeritage-translation-task.html">EUROPEAN LOW RES MULTILINGUAL</A>] \
-  [<A HREF="/wmt21/large-scale-multilingual-translation-task.html">LARGE-SCALE MULTILINGUAL</A>] \
-  [<A HREF="/wmt21/triangular-mt-task.html">TRIANGULAR MT</A>] \
+  [<A HREF="/wmt23/index.html">HOME</A>] \
   <br> \
-  [<A HREF="/wmt21/efficiency-task.html">EFFICIENCY</A>] \
-  [<A HREF="/wmt21/terminology-task.html">TERMINOLOGY</A>] \
-  [<A HREF="/wmt21/unsup_and_very_low_res.html">UNSUP AND VERY LOW RES</A>] \
-  [<A HREF="/wmt21/lifelong-learning-task.html">LIFELONG LEARNING</A>] \
+  TRANSLATION TASKS: \
+  [<A HREF="/wmt23/translation-task.html">GENERAL MT (NEWS)</A>] \
+  [<A HREF="https://wmt-terminology-task.github.io/">TERMINOLOGY</A>] \
+  [<A HREF="/wmt23/word-autocompletion.html">WORD-LEVEL AUTOCOMPLETION</A>] \
   <br> \
   EVALUATION TASKS: \
-  [<A HREF="/wmt21/quality-estimation-task.html">QUALITY ESTIMATION</A>] \
-  [<A HREF="/wmt21/metrics-task.html">METRICS</A>]  \
   <br> \
   OTHER TASKS: \
-  [<A HREF="/wmt21/ape-task.html">AUTOMATIC POST-EDITING</A>] \
 ');
-*/
-/*
-document.write('\
-  [<A HREF="/wmt20/index.html">HOME</A>] \
-  [<A HREF="/wmt20/program.html">SCHEDULE</A>] \
-  [<A HREF="/wmt20/papers.html">PAPERS</A>] \
-  [<A HREF="/wmt20/virtual-conference.html">VIRTUAL CONFERENCE</A>] \
-  [<A HREF="/wmt20/results.html">RESULTS</A>]\
-<br>\
-TRANSLATION TASKS: \
-  [<A HREF="/wmt20/translation-task.html">NEWS</A>] \
-  [<A HREF="/wmt20/lifelong-learning-task.html">LIFELONG LEARNING</A>] \
-  [<A HREF="/wmt20/robustness.html">ROBUSTNESS</A>] \
-  [<A HREF="/wmt20/similar.html">SIMILAR LANGUAGES</A>] \
-  [<A HREF="/wmt20/unsup_and_very_low_res">UNSUP AND VERY LOW RES</A>] \
-[<A HREF="/wmt20/biomedical-translation-task.html">BIOMEDICAL</A>] \
-[<A HREF="/wmt20/chat-task.html">CHAT</A>] \
-<br> \
-OTHER TASKS: \
-  [<A HREF="/wmt20/ape-task.html">AUTOMATIC POST-EDITING</A>] \
-  [<A HREF="/wmt20/metrics-task.html">METRICS</A>] \
-  [<A HREF="/wmt20/quality-estimation-task.html">QUALITY ESTIMATION</A>] \
-  [<A HREF="/wmt20/parallel-corpus-filtering.html">PARALLEL CORPUS FILTERING</A>] \
-');
-*/
 
+/*
+  [<A HREF="/wmt23/index.html">HOME</A>] \
+  [<A HREF="/wmt23/program.html">SCHEDULE</A>] \
+  <br> \
+  TRANSLATION TASKS: \
+  [<A HREF="/wmt23/translation-task.html">GENERAL MT (NEWS)</A>] \
+  [<A HREF="/wmt23/biomedical-translation-task.html">BIOMEDICAL</A>] \
+  [<A HREF="/wmt23/large-scale-multilingual-translation-task.html">LARGE-SCALE MULTILINGUAL AFRICAN</A>] \
+  [<A HREF="/wmt23/efficiency-task.html">EFFICIENCY</A>] \
+  [<A HREF="https://www.wmt-slt.com/">SIGN LANGUAGE</A>] \
+  [<A HREF="/wmt23/code-mixed-translation-task.html">CODE MIXED</A>] \
+  [<A HREF="https://wmt-chat-task.github.io/">CHAT</A>] \
+  [<A HREF="/wmt23/unsup_and_very_low_res.html">UNSUP AND VERY LOW RES</A>] \
+  <br> \
+  EVALUATION TASKS: \
+  [<A HREF="https://wmt-metrics-task.github.io/">METRICS</A>] \
+  [<A HREF="https://wmt-qe-task.github.io/">QUALITY ESTIMATION</A>] \
+  <br> \
+  OTHER TASKS: \
+  [<A HREF="/wmt23/word-autocompletion.html">WORD-LEVEL AUTOCOMPLETION</A>] \
+  [<A HREF="/wmt23/translation-suggestion-task.html">TRANSLATION SUGGESTION</A>] \
+  [<A HREF="/wmt23/ape-task.html">AUTOMATIC POST-EDITING</A>] \
+');
+*/
